247cb2a610d39dd5cf00732351def0a7d4e86358
Heltzel <heltzel.service@hotmail.com>
2020-11-22 11:58:21 +0100

    Project 12-sidebar-modal done

--- 12-sidebar-modal/setup/src/Modal.js
+++ 12-sidebar-modal/setup/src/Modal.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { useGlobalcontext } from './context'
 const Modal = () => {
-  return <h2>Modal</h2>
+  const { isModalOpen, closeModal } = useGlobalcontext()
+  return (
+    <div className={`modal-overlay ${isModalOpen ? 'show-modal' : ''}`}>
+      <div className="modal-container">
+        <h3>modal content</h3>
+        <button className="close-modal-btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Modal
