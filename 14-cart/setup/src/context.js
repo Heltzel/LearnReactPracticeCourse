7ede09e60acb61335b8e19513c94eba069537cf0
Heltzel <heltzel.service@hotmail.com>
2020-11-24 17:27:12 +0100

    Project 14-Cart- upto get totals

--- 14-cart/setup/src/context.js
+++ 14-cart/setup/src/context.js
@@ -23,6 +23,16 @@ const AppProvider = ({ children }) => {
   const removeItem = (id) => {
     dispatch({ type: 'REMOVE_ITEM', payload: id })
   }
+  const increaseAmount = (id) => {
+    dispatch({ type: 'INCREASE_AMOUNT', payload: id })
+  }
+  const decreaseAmount = (id) => {
+    dispatch({ type: 'DECREASE_AMOUNT', payload: id })
+  }
+
+  useEffect(() => {
+    dispatch({ type: 'GET_TOTALS' })
+  }, [state.cart])
 
   return (
     <AppContext.Provider
@@ -30,6 +40,8 @@ const AppProvider = ({ children }) => {
         ...state,
         clearCart,
         removeItem,
+        increaseAmount,
+        decreaseAmount,
       }}
     >
       {children}
