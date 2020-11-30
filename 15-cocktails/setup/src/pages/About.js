ca2307d84385a53f79cc0e04e2350f2d234613fd
Heltzel <heltzel.service@hotmail.com>
2020-11-30 16:48:58 +0100

    Project 15-Cocktails-easy pages

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+        nulla dicta deserunt quas pariatur vero illum omnis eaque beatae error
+        nesciunt quasi ipsa, ex, numquam quo aperiam. Fugit provident neque
+        reiciendis numquam quibusdam ipsum, nam ex aspernatur corrupti sapiente
+        facere?
+      </p>
+    </section>
   )
 }
 
