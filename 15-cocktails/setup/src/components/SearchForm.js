07db6d0154da62f4ce803f039dec4e5b8f574e60
Heltzel <heltzel.service@hotmail.com>
2020-11-30 19:01:31 +0100

    Project 15-Cocktails-search functionality

--- 15-cocktails/setup/src/components/SearchForm.js
+++ 15-cocktails/setup/src/components/SearchForm.js
@@ -3,10 +3,34 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext()
+  const searchValue = React.useRef('')
+
+  const searchCocktail = (params) => {
+    setSearchTerm(searchValue.current.value)
+  }
+
+  React.useEffect(() => {
+    searchValue.current.focus()
+  }, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
